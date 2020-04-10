e5708d80559bb2b3ff4b62370bf55c2231ea4f16
danilozag1992 <danilozagarcanin@gmail.com>
2020-04-10 13:24:05 +0200

    Finished user signup now user can choose profile picture and username to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   def new
+    @user = User.new
+  end
+  def show
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the MVP app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :image
   before_save { self.username = username.downcase }
   validates :username, presence: true, length: {minimum: 3, maximum: 20},
             uniqueness: true
