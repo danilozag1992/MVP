6a9e6e43f13c7ab07acf416e3c151adcb467b13a
danilozag1992 <danilozagarcanin@gmail.com>
2020-04-27 14:16:43 +0200

    Added validations to groups and  practices

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,6 @@ class Group < ApplicationRecord
   has_many :practice_groups
   has_many :practices, -> { distinct }, through: :practice_groups, dependent: :destroy
   before_save { self.name = name.downcase}
-  validates :name, presence: true, case_sensitive: false
+  validates :name, presence: true,length: {minimum: 3, maximum: 20} ,case_sensitive: false, numericality: false
+  validates_format_of :name, :with => /^[a-z]+$/i, :multiline => true
 end
